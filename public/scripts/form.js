b8ca69b19c2f6c61824ff6f548c2b0466b0a42f4
kaduhod <carlosjr.ribas@gmail.com.br>
2026-04-19 10:39:25 -0300

    Refatorado form.ejs e form.js

--- public/scripts/form.js
+++ public/scripts/form.js
@@ -1,46 +1,24 @@
-const form = () => document.getElementById('form-<%= entidade.nome %>');
-const desktopContainer = () => document.getElementById('itens-container-desktop');
-const mobileContainer  = () => document.getElementById('itens-container-mobile');
-const isMobile = () => window.innerWidth < 768;
+const FORM = () => document.getElementById('form-<%= entidade.nome %>');
+const ITENS_CONTAINER = () => document.getElementById('itens-container');
 function replaceAtributosParaNovoItem(text, id) {
     return text
-            .replaceAll(/value="[^"]*"/g, 'value')
-            .replaceAll(/hx-\w+="[^"]*"/g, "")
-            .replaceAll(/data-item-pk="true">[^<]*</g, "data-item-pk='true'><")
-            .replaceAll(/data-item-label="true">[^<]*</g, "><")
-            .replaceAll(/data-id="[^"]*"/g, `data-id='${id}'`)
-            .replaceAll(/data-remove-input="true"/g, `data-remove-input="false"`);
+            .replaceAll(/value="[^"]*"/g, 'value=""')
+            .replace(/(<[^>]*hx-get[^>]*class="[^"]*)\bcursor-pointer\b([^"]*")/g, '$1$2').replaceAll(/hx-\w+="[^"]*"/g, "")
+            .replaceAll(/data-item-pk="true">[^<]*</g, "data-item-pk='true'>Novo<")
+            .replaceAll(/data-item-label="true">[^<]*</g, ">...<")
+            .replaceAll(/data-id="[^"]*"/g, `data-id='${id}'`);
 }
-function gerencia_excluir() {
-    let input_excluir = this.parentNode.querySelector('input[data-remove-item="true"]');
-    if(!input_excluir) {
-        alert("Erro gravar exclusão de item");
-        this.checked = false;
-        return;
-    }
-    input_excluir.value = this.checked ? 1 : 0;
-}
+
 function adicionar_item() {
     // Mobile
-    const primeiro_item_desktop = document.querySelector('tr[data-desktop-item="true"]');
-    const primeiro_item_mobile = document.querySelector('div[data-mobile-item="true"]');
-    if(primeiro_item_mobile.innerHTML && primeiro_item_desktop.innerHTML) {
+    const primeiro_item = document.querySelector('div[data-item="true"]');
+    if(primeiro_item.innerHTML) {
         const novo_id = "untracked_"+Math.random().toString(36).substring(2, 10);
-        desktopContainer().innerHTML += replaceAtributosParaNovoItem(primeiro_item_desktop.outerHTML, novo_id);
-        mobileContainer().innerHTML += replaceAtributosParaNovoItem(primeiro_item_mobile.outerHTML, novo_id);
-
-        let novo_item_desktop = desktopContainer().querySelector(`tr[data-id="${novo_id}"]`);
-        let novo_item_mobile = mobileContainer().querySelector(`div[data-id="${novo_id}"]`);
-
-        novo_item_desktop.querySelector('input[data-remove-item="true"]').addEventListener("change", gerencia_excluir);
-        novo_item_mobile.querySelector('input[data-remove-item="true"]').addEventListener("change", gerencia_excluir);
+        ITENS_CONTAINER().innerHTML = replaceAtributosParaNovoItem(primeiro_item.outerHTML, novo_id) + ITENS_CONTAINER().innerHTML;
+        // let novo_item = ITENS_CONTAINER.querySelector(`div[data-id="${novo_id}"]`);
     }
 }
 export default function loadEvents() {
-    const add_item = document.getElementById("adicionar-item");
-    add_item.addEventListener('click', adicionar_item);
-    const checkboxes_remover = document.querySelectorAll('input[data-remove-item="true"]');
-    if(checkboxes_remover) {
-        checkboxes_remover.forEach(b => b.addEventListener("change", gerencia_excluir));
-    }
+    const add_item = document.querySelectorAll("button[data-id='adicionar-item'");
+    add_item.forEach(e => e.addEventListener('click', adicionar_item));
 }
